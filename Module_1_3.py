f3d89eae7adb7fd160a3f5654be6bc634a98dadf
EkaBond <ekabond87@gmail.com>
2024-10-14 19:12:34 +0300

    Add files via upload

---
+++ Module_1_3.py
@@ -0,0 +1,12 @@
+name  = 'Katish'
+print('Name: ' + name)
+
+age = 43
+print('Age: ' + str(age))
+
+age = age + 1
+print('New age: ' + str(age))
+
+
+is_student = True
+print('Is student: ' + str(is_student))
